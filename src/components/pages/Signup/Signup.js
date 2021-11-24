3c285d7ff188e60ec92c63cbd294f12241113801
Leonardo <leodscarvalho@gmail.com>
2021-11-23 22:25:18 -0300

    adding input info

--- src/components/pages/Signup/Signup.js
+++ src/components/pages/Signup/Signup.js
@@ -1,4 +1,5 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useReducer } from 'react';
+import inputData from './inputData.js';
 import SignupSVG from '@assets/signup.svg';
 import { useFetch } from '@components/hooks.js';
 
@@ -19,6 +20,53 @@ const Message = (props) => {
 		<section className={`message ${type}`}>
 			<p>{content}</p>
 		</section>
+	)
+}
+
+const SignupInfo = (props) => {
+	const { name, type, placeholder, pattern, minLength, maxLength, requirements } = props.content;
+	const [inputValue, setInputValue] = useState('');
+	const [requirementsDisplay, setRequirementsDisplay] = useState("block");
+
+	const reqInfoCtn = useRef(null);
+
+	useEffect(() => {
+		const infosRight = reqInfoCtn.current.querySelectorAll('.right');
+		if (infosRight.length == requirements.length) {
+			setRequirementsDisplay("none");
+		} else setRequirementsDisplay("block");
+	}, [inputValue]);
+
+	return (
+		<>
+			<label htmlFor={name}></label>
+			<input name={name} minLength={minLength} maxLength={maxLength}
+				pattern={pattern} type={type} placeholder={placeholder} 
+				onChange={(e) => setInputValue(e.target.value)}/>
+			<div ref={reqInfoCtn} className="req-info-cnt" style={{display: requirementsDisplay}}>
+				{inputValue.length > 0 && requirements.map(req => 
+					{ return <Requirement text={req.name} inputValue={inputValue} isValid={req.checkValidity}/> })}
+			</div>
+		</>
+	)
+}
+
+const Requirement = (props) => {
+	const { text, inputValue, isValid} = props;
+	const para = useRef(null);
+
+	useEffect(() => {
+		if (isValid(inputValue)) {
+			para.current.classList.remove("wrong");
+			para.current.classList.add("right");
+		} else {
+			para.current.classList.remove("right");
+			para.current.classList.add("wrong");
+		}
+	}, [inputValue]);
+
+	return (
+		<p ref={para} className="input-info wrong">{text}</p>	
 	)
 }
 
@@ -60,14 +108,7 @@ const Signup = () => {
 			<p className="subtitle">E veja o quanto se dedica a cada tarefa ao longo do seu dia!</p>
 			{message.status && <Message type={message.type} content={message.content} />}
 			<form ref={form} autoComplete="off">
-				<label htmlFor="name"></label>
-				<input name="name" placeholder="Nome" minLength="3" maxLength="20"/>
-				<label htmlFor="email"></label>
-				<input name="email" placeholder="Email" type="email"/>
-				<label htmlFor="password"></label>
-				<input name="password" placeholder="Senha" type="password"/>
-				<label htmlFor="confirm-password"></label>
-				<input name="confirm-password" placeholder="Confirmar Senha" type="password"/>
+				{inputData.map(data => { return <SignupInfo content={data} /> })}
 				<button className="confirm" onClick={(e) => sendRequest(e)}>Cadastrar</button>
 			</form>
 		</section>
---
+++ src/components/pages/Signup/inputData.js
@@ -0,0 +1,103 @@
+const data = [
+  {
+    name: "name",
+    type: "text",
+    placeholder: "Nome",
+    minLength: 3,
+    maxLength: 15,
+    pattern: "[a-zA-Z]+",
+    requirements: [
+      {
+        name: "apenas letras",
+        checkValidity: function (content) {
+          const result = content.match("[a-zA-Z]+");
+          if (result !== null && result[0].length === content.length)
+            return true;
+          return false;
+        },
+      },
+      {
+        name: "Minimo de 3 letras",
+        checkValidity: function (content) {
+          if (content.length >= 3) return true;
+          return false;
+        },
+      },
+    ],
+  },
+  {
+    name: "email",
+    type: "email",
+    placeholder: "Email",
+    minLength: null,
+    maxLength: null,
+    pattern: null,
+    requirements: [
+      {
+        name: "email invalido",
+        checkValidity: function () {},
+      },
+    ],
+  },
+  {
+    name: "password",
+    type: "password",
+    placeholder: "Senha",
+    minLength: 8,
+    maxLength: 25,
+    pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,25}$",
+    requirements: [
+      {
+        name: "Mínimo de 8 caracteres",
+        checkValidity: function (content) {
+          if (content.length >= 8) return true;
+          return false;
+        },
+      },
+      {
+        name: "1 letra minúscula",
+        checkValidity: function (content) {
+          if (content.match("[a-z]+")) return true;
+          return false;
+        },
+      },
+      {
+        name: "1 letra maiúscula",
+        checkValidity: function (content) {
+          if (content.match("[A-Z]+")) return true;
+          return false;
+        },
+      },
+      {
+        name: "1 caracter especial",
+        checkValidity: function (content) {
+          if (content.match("[!@#$%&¨*_=]+")) return true;
+          return false;
+        },
+      },
+      {
+        name: "1 digito",
+        checkValidity: function (content) {
+          if (content.match("[0-9]+")) return true;
+          return false;
+        },
+      },
+    ],
+  },
+  {
+    name: "confirm-password",
+    type: "password",
+    placeholder: "Confirmar Senha",
+    minLength: null,
+    maxLength: 25,
+    pattern: null,
+    requirements: [
+      {
+        name: "senha e confirmação diferentes",
+        checkValidity: function (content) {},
+      },
+    ],
+  },
+];
+
+export default data;
